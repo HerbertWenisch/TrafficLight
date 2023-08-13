e1aacf2eb7d5b4200420a60e1d6a79689c51e059
HerbertWenisch <herbert.wenisch@t-online.de>
2023-08-13 11:48:26 +0200

    "Version 1.0: with code of display"

--- TrafficLight.java
+++ TrafficLight.java
@@ -6,22 +6,39 @@
 import java.awt.Color;
 
 public class TrafficLight {
+    private final String version = "1.0";
     // Colors:
     private final Color green = new Color(0,50,0);
     private final Color red = Color.red;
     private final Color gray = Color.gray;
     // states: red (normal state), green
     private String state; // current state
+    // lamps:
+    private Lamp lampUpper;
+    private Lamp lampLower;
     
     public TrafficLight(){
        state = "red";
-       // Your code!
-       
+       lampUpper = new Lamp(0, 80, red);
+       lampLower = new Lamp(0, 30, gray);
+       display();
     }
     
     
     public void display(){
-        // Your code!
+        // frame:
+        Turtlegrafik.turtle.setPenColor(Color.black);
+        Turtlegrafik.turtle.setPenWidth(2);
+        Turtlegrafik.turtle.setPos(30,0);
+        for(int i = 0; i < 2; i++){
+           Turtlegrafik.turtle.fd(110);
+           Turtlegrafik.turtle.lt(90);
+           Turtlegrafik.turtle.fd(60);
+           Turtlegrafik.turtle.lt(90);
+        }
+        // lamps:
+        lampUpper.display();
+        lampLower.display();
     }
            
 }
